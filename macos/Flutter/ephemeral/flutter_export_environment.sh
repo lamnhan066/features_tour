c64a679ef3e2cd1c2774a077d80168a1a9fb3dd9
lamnhan066 <hello@lamnhan.dev>
2024-07-13 23:22:49 +0700

    Chore

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/thanhnhan/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/thanhnhan/Documents/Development/Projects/Flutter Plugin/features_tour"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.4.5"
+export "FLUTTER_BUILD_NUMBER=0.4.5"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
